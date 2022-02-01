79a6e162a201c09b2463515a7c8be6f1954a9367
Mostafa ElSheikh <mostafa.elsheikh@vodafone.com>
2022-02-01 22:30:09 +0200

    fix: container port and target port, add public url of apiHost in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://reverseproxy:8080/api/v0'
+  apiHost: 'http://aaf626194cc2e49acba8321b5f8a1d45-1237303019.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
